dd7d6d580c11e919742bb078ef8d2bcfd9664ec7
seohsj <hyunju6889@naver.com>
2021-04-13 01:26:08 +0900

    remove unnecessary folder

--- ML/re_Train_suhyun/data_check.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-path = "Sign-Language-Digits-Dataset/Dataset/"
-file_list = os.listdir(path)
-file_list.sort()
-for i in file_list:
-    temp_file_list=os.listdir(path+i)
-    print(i," : ",len(temp_file_list))
--- Web/README.md
+++
@@ -1,3 +0,0 @@
-# Handlang
-
-## DSC Ewha
