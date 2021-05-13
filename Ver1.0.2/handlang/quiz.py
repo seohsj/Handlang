e8dadd589b52020c3b0522699f58f071a88a8cd2
seohsj <hyunju6889@naver.com>
2021-05-13 23:02:30 +0900

    퀴즈 맞은 개수 세기 코드 변경

--- Ver1.0.2/handlang/quiz.py
+++ Ver1.0.2/handlang/quiz.py
@@ -20,47 +20,46 @@ def _list(group):
         question_list= make_quiz(group)
         print(question_list)
         return render_template('quiz/list.html',group=group, enumerate=enumerate, question_list=question_list,
-                                total_q=g.number_of_quiz , link=request.full_path)
+                                total_q=g.number_of_quiz)
+
 
     if request.method == 'POST':
-        user_answers = {}
+        incorrect_questions = []
+        correct_num = 0
         for i in range(g.number_of_quiz ):
             question = "question" + str(i)
             answer = "answer" + str(i)
             q = request.form[question]
             a = request.form[answer]
-            user_answers[q] = a
-            print(user_answers)
-        user_answers = json.dumps(user_answers)
+            if(q==a):
+                correct_num += 1
+            else:
+                incorrect_questions.append(q.upper())
 
-        return redirect(url_for('quiz.result',group=group, user_answers=user_answers))
+
+        incorrect_questions = json.dumps(incorrect_questions)
+        return redirect(url_for('quiz.result',group=group, correct_num = correct_num , incorrect_questions=incorrect_questions))
 
 
-@bp.route('/<group>/result')
+@bp.route('/<group>/result')  #http://127.0.0.1:5000/quiz/number/result?correct_num=0&incorrect_questions=%5B%227%22%2C+%225%22%2C+%223%22%2C+%226%22%2C+%222%22%5D
 def result(group):
     try:
-        user_answers = json.loads(request.args['user_answers'])
-        items = user_answers.items()
-    except:
-        user_answers = {}
-    items = user_answers.items()
-    correct_num = 0
-    incorrect_questions = []
-    for q, a in items:
-        if (q == a):
-            correct_num += 1
-        else:
-            incorrect_questions.append(q.upper())
+        incorrect_questions = json.loads(request.args['incorrect_questions'])
+        correct_num = int(request.args['correct_num'])
 
-    if correct_num == g.number_of_quiz :
+    except:
+        #예외처리
+          incorrect_questions = []
+          correct_num=0
+ 
+    if correct_num == g.number_of_quiz :  #점수에 따라서 그림을 다르게 하는 것
         img_path = "score_100.png"
     elif correct_num >= (g.number_of_quiz  // 2):
         img_path = "score_50.png"
     else:
         img_path = "score_0.png"
-    print(img_path)
     return render_template('quiz/result.html', group=group,correct_num=correct_num, incorrect_questions=incorrect_questions,
-                           total_q=g.number_of_quiz , img_path=img_path, link=request.full_path)
+                           total_q=g.number_of_quiz , img_path=img_path)
 
 
 def make_quiz(group):
@@ -71,7 +70,7 @@ def make_quiz(group):
     except ValueError:
         print("전체 질문 수(number_of_quiz)가 ", len(letter_list),"를 넘습니다.")  
         return redirect(url_for('home.index'))  #https://flask.palletsprojects.com/en/2.0.x/errorhandling/
-    
+
     quiz_list={}
     for question in questions:
         choices= make_random_choices(letter_list, question)
@@ -92,4 +91,5 @@ def make_random_choices(letter_list, question):
     random.shuffle(choices)
     
     return choices
+
 
